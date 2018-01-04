2b0883d442193dd4d1c0021adb46aafa4194deea
sstone <sstone72389@gmail.com>
2018-01-04 14:59:52 -0500

    adds dropdowns for country and state

--- app.js
+++ app.js
@@ -10,11 +10,13 @@ function initAutocomplete() {
 		console.log(placeSelected);
 		document.querySelector('[data-id=output]').innerHTML = placeSelected.adr_address;
 
-document.querySelector('[data-id=address]').value = placeSelected.name;
+document.querySelector('[data-type=autocomplete]').value = placeSelected.address_components[0].long_name + " " + placeSelected.address_components[1].short_name;
 
-document.querySelector('[data-id=city]').value = placeSelected.address_components[2].long_name;
+document.querySelector('[data-id=city]').value = placeSelected.vicinity;
 
-document.querySelector('[data-id=state]').value = placeSelected.address_components[4].long_name;
+document.querySelector('[data-id=state]').value = placeSelected.address_components[4].short_name;
+
+document.querySelector('[data-id=country]').value = placeSelected.address_components[5].short_name;
     document.querySelector('[data-id=zip]').value = placeSelected.address_components[6].long_name;
 
 });
