6c06be6e1a0ae35415ea159f1b8e9bbde81795b7
sstone <sstone72389@gmail.com>
2018-01-18 11:25:16 -0500

    updates JS logicx to reflect geolocation and correct postal code for Canadian addresses

--- app.js
+++ app.js
@@ -1,23 +1,55 @@
+var placeSearch, autocomplete;
+
+// List all address components as laid out in googles address object
+var componentForm = {
+  autocomplete: ['street_number', 'route'],
+  inputCity: 'locality',
+  inputState: 'administrative_area_level_1',
+  inputZip: 'postal_code',
+  inputCountry: 'country'
+};
+
+// autocomplete object
+
 function initAutocomplete() {
-	const input = document.querySelector('[data-type=autocomplete]');
-	const autocomplete = new google.maps.places.Autocomplete(
-      input,
-      { types: ['geocode'] }
-	);
+  autocomplete = new google.maps.places.Autocomplete(
 
-	autocomplete.addListener('place_changed', () => {
-		const placeSelected = autocomplete.getPlace();
-		console.log(placeSelected);
-		document.querySelector('[data-id=output]').innerHTML = placeSelected.adr_address;
+(document.getElementById('autocomplete')),
+      {type: ['geocode']});
 
-document.querySelector('[data-type=autocomplete]').value = placeSelected.address_components[0].long_name + " " + placeSelected.address_components[1].short_name;
+  // Call fillInAddress when user selects an address from dropdown
+  autocomplete.addListener('place_changed', fillInAddress);
+}
 
-document.querySelector('[data-id=city]').value = placeSelected.vicinity;
+// Fill fields with corresponding values
+function fillInAddress() {
 
-document.querySelector('[data-id=state]').value = placeSelected.address_components[4].short_name;
+  // Get place data from autocomplete object
+  var place = autocomplete.getPlace();
+  console.log(place);
 
-document.querySelector('[data-id=country]').value = placeSelected.address_components[5].short_name;
-    document.querySelector('[data-id=zip]').value = placeSelected.address_components[6].long_name;
+  // Enable each field, then fill them with the corresponding value from the place object
+  for (var component in componentForm) {
+    document.getElementById(component).disabled = false;
+    document.getElementById(component).value = search(componentForm[component], place.address_components);
+  }
 
-});
+  // Fill the autocomplete field with values from the place object
+  // If a street # is not found, set the field to route only.
+  if (search("street_number", place.address_components) != "") {
+    document.getElementById("autocomplete").value = search("street_number", place.address_components) + " ";
+  }
+  document.getElementById("autocomplete").value += search("route", place.address_components);
+
+  // Search the passed object for a specified address component/type and return value of the matched component.
+  // If requested type does not exist, return empty string
+  function search(type, placeObject) {
+    for (var i = 0; i < placeObject.length; i++) {
+      if (placeObject[i].types[0] === type) {
+        return placeObject[i].short_name;
+      } else if (i === placeObject.length - 1) {
+        return "";
+      }
+    }
+  }
 }
